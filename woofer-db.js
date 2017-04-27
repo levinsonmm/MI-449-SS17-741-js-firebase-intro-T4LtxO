4902400804e642f894537afe70c3bb0673401303
levinsonmm <levinsonmm@gmail.com>
2017-04-26 21:01:19 -0400

    filled out functions in woofer-db

--- woofer-db.js
+++ woofer-db.js
@@ -1,8 +1,23 @@
 // TODO Sign into the database anonymously
 
+var config = {
+  apiKey: 'AIzaSyC5Xv1ZlJ1Gs1McIJO_7fQxHHVktLcVSkM',
+  authDomain: 'first-ade7d.firebaseapp.com',
+  databaseURL: 'https://first-ade7d.firebaseio.com',
+  projectId: 'first-ade7d',
+  storageBucket: 'first-ade7d.appspot.com',
+  messagingSenderId: '76066012979'
+}
+firebase.initializeApp(config)
+firebase.auth().signInAnonymously()
+
 // CREATE a new woof in Firebase
 function createWoofInDatabase (woof) {
-  // TODO create a new record in Firebase
+  // create a new record in Firebase
+  firebase.database().ref('woofs').push({
+    created_at: woof.created_at,
+    text: woof.text
+  })
 }
 
 // READ from Firebase when woofs are added, changed, or removed
@@ -11,17 +26,30 @@ function createWoofInDatabase (woof) {
 // sure to pass the right parameters (hint: these functions are
 // defined in woofer-ui.js).
 function readWoofsInDatabase () {
-  // TODO read new, changed, and deleted Firebase records
+  // read new, changed, and deleted Firebase records
+  firebase.database().ref('woofs').on('child_added', function (newWoofSnapshot) {
+    addWoofRow(newWoofSnapshot.key, newWoofSnapshot.val())
+  })
+  firebase.database().ref('woofs').on('child_changed', function (updatedWoofSnapshot) {
+    updateWoofRow(updatedWoofSnapshot.key, updatedWoofSnapshot.val())
+  })
+  firebase.database().ref('woofs').on('child_removed', function (deletedWoofSnapshot) {
+    deleteWoofRow(deletedWoofSnapshot.key, deletedWoofSnapshot.val())
+  })
 }
 
 // UPDATE the woof in Firebase
 function updateWoofInDatabase (woofKey, woofText) {
-  // TODO update the record in Firebase
+  // update the record in Firebase
+  firebase.database().ref('woofs').child(woofKey).child('text').set(
+    woofText
+  )
 }
 
 // DELETE the woof from Firebase
 function deleteWoofFromDatabase (woofKey) {
-  // TODO delete the record from Firebase
+  // delete the record from Firebase
+  firebase.database().ref('woofs').child(woofKey).remove()
 }
 
 // Load all of the data
